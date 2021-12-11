1117865e383eecf41809cf34cbb4d6fe36378e71
Toofifty <amatho250@gmail.com>
2021-12-12 00:59:03 +1100

    idiota

--- src/core/kk-context.tsx
+++ src/core/kk-context.tsx
@@ -9,21 +9,7 @@ const KKContext = createContext<{
 export const useKKContext = () => useContext(KKContext);
 
 export const KKProvider = ({ children }: { children: React.ReactNode }) => {
-  const [kkState, setKKState] = useState<KrisKringle>({
-    people: {
-      '1': 'Apple',
-      '2': 'Banana',
-      '3': 'Cherry',
-      '4': 'Date',
-      '5': 'Elderberry',
-      '6': 'Fig',
-      '7': 'Grape',
-      '8': 'Honeydew',
-      '9': 'Jackfruit',
-      '10': 'Kiwi',
-      '11': 'Lemon',
-    },
-  });
+  const [kkState, setKKState] = useState<KrisKringle>({});
 
   return (
     <KKContext.Provider value={{ kk: kkState, setKK: setKKState }}>
